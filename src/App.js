ad7c7855eff36a3caa437e4010b278b24cb1c0a6
matinouw <matinou100@hotmail.fr>
2021-12-14 20:17:34 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import 'react-calendar/dist/Calendar.css';
 
 
 function Todo({ todo, index, completeTodo, removeTodo }) {
+  
   return (
     <div
       className="todo"
       style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      
     >
       {todo.text}
+      {todo.date.toString()}
       <div>
         <button onClick={() => completeTodo(index)}>Done</button>
         <button onClick={() => removeTodo(index)}>Skip</button>
@@ -46,20 +49,23 @@ function App() {
   const [todos, setTodos] = React.useState([
     {
       text: "Tech talk session #18 : 'What is cryptocurrency?'",
-      isCompleted: false
+      isCompleted: false,
+      date: new Date()
     },
     {
       text: "Tech talk session #19 : 'How to manage your asset wallet?'",
-      isCompleted: false
+      isCompleted: false,
+      date: new Date()
     },
     {
       text: "Tech talk session #20 : 'What is React and how easy is it to use it?'",
-      isCompleted: false
+      isCompleted: false,
+      date: new Date()
     }
   ]);
 
-  const addTodo = text => {
-    const newTodos = [...todos, { text }];
+  const addTodo = (text, date) => {
+    const newTodos = [...todos, { text, date } ];
     setTodos(newTodos);
   };
 
@@ -77,9 +83,42 @@ function App() {
 
   const [date, setDate] = useState(new Date());
 
+
+  const [name, setName] = useState('');
+  const [pwd, setPwd] = useState('');
+
+  const handle = () => {
+     localStorage.setItem('session_title', name);
+     localStorage.setItem('session_description', pwd);
+     localStorage.setItem('chosen_date', date);
+     addTodo(name, date)
+  };
+  const remove = () => {
+     localStorage.removeItem('session_title');
+     localStorage.removeItem('session_description');
+     localStorage.removeItem('chosen_date', date);
+  };
+
   return (
     <div className="app">
-      <h1 className='text-center'>React Calendar</h1>
+      <h1 className='text-center'>Tech Talk Planning</h1>
+
+
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <p> Choose a date on the calendar and enter the content of your talk session. Then press ENTER to validate. </p>
+        <TodoForm addTodo={addTodo} />
+      </div>
+      
 
       <div className='calendar-container'>
       <Calendar onChange={setDate} value={date} />
@@ -89,19 +128,63 @@ function App() {
       </p>
       </div>
 
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-          />
-        ))}
-        <TodoForm addTodo={addTodo} />
-      </div>
+
+      <div className="App">
+          <h3>Chosen Date:</h3>
+          <span className='bold'></span>{' '}
+          {date.toDateString()}
+          {(e) => setDate(e.target.date.toDateString())}
+
+         <h3>Enter the title of the session:</h3>
+         <input
+            placeholder="Session title"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+         />
+         <h3>Description:</h3>
+         <input
+            placeholder="Session description"
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+         />
+         <div>
+           <h1> Result </h1>
+         </div>
+
+
+         {localStorage.getItem('chosen_date') && (
+            <div>
+               Chosen date: <p>{localStorage.getItem('chosen_date')}</p>
+            </div>
+         )}
+
+         {localStorage.getItem('session_title') && (
+            <div>
+              Session title: <p>{localStorage.getItem('session_title')}</p>
+            </div>
+         )}
+         {localStorage.getItem('session_description') && (
+            <div>
+               Session description: <p>{localStorage.getItem('session_description')}</p>
+            </div>
+         )}
+
+
+        <div>
+            <button onClick={handle}>Done</button>
+         </div>
+
+         <div>
+            <button onClick={remove}>Remove</button>
+         </div>
+         </div>
+
+
+
     </div>
+
+
+
 
     
   );
